c1c1e7ddfe1718e7d0e28064d0bee0bf7a2b1c78
lov3rbo9 <trubslava@gmail.com>
2025-05-28 13:48:21 +0300

    s

--- proj_code/validators/login_validator.py
+++ proj_code/validators/login_validator.py
@@ -2,7 +2,7 @@ import re
 
 def login_validator(login):
     if login is None or len(login) < 5:
-        return "Пароль должен содержать как минимум 5 символов!"
+        return "Логин должен содержать как минимум 5 символов!"
     allowed_characters = r'^[A-Za-z0-9]+$'
     if not re.match(allowed_characters, login):
         return r"Используются запрещенные символы! Вводите только латинские буквы, цифры"
