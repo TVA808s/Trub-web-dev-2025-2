2436b3c835a21378578b6a31161977f9630c8f5e
lov3rbo9 <trubslava@gmail.com>
2025-06-03 15:47:48 +0300

    s

--- proj_code/repositories/log_repository.py
+++ proj_code/repositories/log_repository.py
@@ -9,6 +9,30 @@ class LogRepository:
             cursor.execute(query, (path, user_id))
             connection.commit()
 
+    def get_all_logs(self, page=1, per_page=10):
+        offset = (page - 1) * per_page
+        with self.db_connector.connect().cursor(named_tuple=True) as cursor:
+            # Основной запрос с пагинацией
+            query = """
+            SELECT 
+                vl.id, 
+                vl.path, 
+                vl.created_at,
+                CONCAT(u.last_name, ' ', u.first_name, ' ', u.middle_name) AS user_full_name
+            FROM visit_logs vl
+            LEFT JOIN users u ON vl.user_id = u.id
+            ORDER BY vl.created_at DESC
+            LIMIT %s OFFSET %s
+            """
+            cursor.execute(query, (per_page, offset))
+            logs = cursor.fetchall()
+            
+            # Получение общего количества записей для пагинации
+            cursor.execute("SELECT COUNT(*) AS total FROM visit_logs")
+            total = cursor.fetchone().total
+            
+        return logs, total
+    
     def get_user_logs(self, user_id, page=1, per_page=10):
         offset = (page - 1) * per_page
         with self.db_connector.connect().cursor(named_tuple=True) as cursor:
